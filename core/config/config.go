749abf3256f5956542c1386c74ff9f7ecac799fb
printfcoder <i@shux.me>
2025-02-20 01:35:54 +0800

    add config

--- core/config/config.go
+++ core/config/config.go
@@ -1,28 +1,94 @@
+// Package config is an interface for dynamic configuration.
 package config
 
-/***
-yml:
-first-level:
-  second-level:
-    third-level: value
-    third-level-2: value-2
-  second-level-2:
-    third-level: value
-    third-level-2: value-2
----
-Option
+import (
+	"context"
 
-type config struct {
-	firstLevel struct {
-		secondLevel struct {
-			thirdLevel string
-			thirdLevel2 string
-		} json:"second-level"
-		secondLevel2 struct {
-			thirdLevel string
-			thirdLevel2 string
-		} json:"second-level-2"
-	} json:"first-level"
+	"github.com/dirty-bro-tech/peers-touch-go/core/config/loader"
+	"github.com/dirty-bro-tech/peers-touch-go/core/config/reader"
+	"github.com/dirty-bro-tech/peers-touch-go/core/config/source"
+	"github.com/dirty-bro-tech/peers-touch-go/core/config/source/file"
+)
+
+// Config is an interface abstraction for dynamic configuration
+type Config interface {
+	// provide the reader.Values interface
+	reader.Values
+	// Stop the config loader/watcher
+	Close() error
+	// Load config sources
+	Load(source ...source.Source) error
+	// Force a source changeset sync
+	Sync() error
+	// Watch a value for changes
+	Watch(path ...string) (Watcher, error)
 }
 
-*/
+// Watcher is the config watcher
+type Watcher interface {
+	Next() (reader.Value, error)
+	Stop() error
+}
+
+type Options struct {
+	Loader loader.Loader
+	Reader reader.Reader
+	Source []source.Source
+
+	// for alternative data
+	Context context.Context
+}
+
+type Option func(o *Options)
+
+var (
+	// Default Config Manager
+	DefaultConfig = NewConfig()
+)
+
+// NewConfig returns new config
+func NewConfig(opts ...Option) Config {
+	return newConfig(opts...)
+}
+
+// Return config as raw json
+func Bytes() []byte {
+	return DefaultConfig.Bytes()
+}
+
+// Return config as a map
+func Map() map[string]interface{} {
+	return DefaultConfig.Map()
+}
+
+// Scan values to a go type
+func Scan(v interface{}) error {
+	return DefaultConfig.Scan(v)
+}
+
+// Force a source changeset sync
+func Sync() error {
+	return DefaultConfig.Sync()
+}
+
+// Get a value from the config
+func Get(path ...string) reader.Value {
+	return DefaultConfig.Get(path...)
+}
+
+// Load config sources
+func Load(source ...source.Source) error {
+	return DefaultConfig.Load(source...)
+}
+
+// Watch a value for changes
+func Watch(path ...string) (Watcher, error) {
+	return DefaultConfig.Watch(path...)
+}
+
+// LoadFile is short hand for creating a file source and loading it
+func LoadFile(path string) error {
+	return Load(file.NewSource(
+		file.WithPath(path),
+	))
+}
---
+++ core/config/source/cli/options.go
@@ -0,0 +1,20 @@
+package cli
+
+import (
+	"context"
+
+	"github.com/dirty-bro-tech/peers-touch-go/core/config/cli"
+	"github.com/dirty-bro-tech/peers-touch-go/core/config/source"
+)
+
+type contextKey struct{}
+
+// Context sets the cli context
+func Context(c *cli.Context) source.Option {
+	return func(o *source.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, contextKey{}, c)
+	}
+}
