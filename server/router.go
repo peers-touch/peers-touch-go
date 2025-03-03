bae3d6d564048fdfd68afafdd950fa2e737451f1
printfcoder <i@shux.me>
2025-03-03 21:11:45 +0800

    router define

--- server/router.go
+++ server/router.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"net/http"
-	
 	"github.com/dirty-bro-tech/peers-touch-go/core/server"
 )
 
@@ -12,6 +10,10 @@ import (
 // if you what to register a handler with Peers server, you can implement this interface, then call server.Handle() to register it.
 type Router interface {
 	server.Handler
+}
+
+type Routers interface {
+	Routers() []Router
 }
 
 type ActivityPubRouterURL string
@@ -24,60 +26,3 @@ const (
 	ActivityPubRouterURLLike     ActivityPubRouterURL = "/like"
 	ActivityPubRouterURLUndo     ActivityPubRouterURL = "/undo"
 )
-
-type ActivityPubRouter struct{}
-
-func (r *ActivityPubRouter) handleInbox(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Handle incoming activities
-}
-
-func (r *ActivityPubRouter) handleOutbox(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Handle outgoing activities
-}
-
-func (r *ActivityPubRouter) handleActor(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Return actor information
-}
-
-func (r *ActivityPubRouter) handleFollowers(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Return followers collection
-}
-
-func (r *ActivityPubRouter) handleFollowing(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Return following collection
-}
-
-func (r *ActivityPubRouter) handleLiked(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Return liked activities
-}
-
-func (r *ActivityPubRouter) handleShares(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Return shared activities
-}
-
-func (r *ActivityPubRouter) handleBlocks(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Handle blocked users
-}
-
-func (r *ActivityPubRouter) handleMutes(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Handle muted users
-}
-
-func (r *ActivityPubRouter) handleReports(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/activity+json")
-	// Handle reports
-}
-
-// NewActivityPubRouter creates a new router with ActivityPub endpoints
-func NewActivityPubRouter() *ActivityPubRouter {
-	return &ActivityPubRouter{}
-}
