3307134da99ad0ea2c33d2839719f7ba3d0b763c
printfcoder <printfcoder@gmail.com>
2025-05-24 23:51:58 +0800

    add user model

---
+++ model/db/user.go
@@ -0,0 +1,13 @@
+package db
+
+import "time"
+
+type User struct {
+	ID           uint64 `gorm:"primary_key;autoIncrement:false"` // Snowflake ID
+	Name         string `gorm:"size:100;not null"`               // User's display name
+	Email        string `gorm:"uniqueIndex;size:255;not null"`   // Unique email address
+	PasswordHash string `gorm:"size:128;not null"`               // bcrypt hashed password
+
+	CreatedAt time.Time `gorm:"created_at"`
+	UpdatedAt time.Time `gorm:"updated_at"`
+}
